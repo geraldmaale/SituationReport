3db3c84230d971d4b94a609dacad9271fb65d752
Gerald Maale <55647505+geraldmaale@users.noreply.github.com>
2023-04-08 00:07:54 +0800

    Bumped to .NET 7

--- src/SR.Components/Pages/Account/ChangePassword.razor.cs
+++ src/SR.Components/Pages/Account/ChangePassword.razor.cs
@@ -1,42 +1,40 @@
-﻿using GreatIdeas.Blazor.Extensions;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using SR.Components.DataServices;
 using SR.Components.Helpers;
 using SR.Shared.DTOs.User;
 
 namespace SR.Components.Pages.Account
 {
-    public class ChangePasswordBase : ServiceComponentBase<ChangePasswordBase>
-    {
-        [Inject] public IUserDataService AccountDataService { get; set; }
+	public class ChangePasswordBase : ServiceComponentBase<ChangePasswordBase>
+	{
+		[Inject] public IUserDataService AccountDataService { get; set; }
 
-        public ChangePasswordDto ChangePasswordModel { get; set; } = new();
+		public ChangePasswordDto ChangePasswordModel { get; set; } = new();
 
-        
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-	            var authenticationState = await AuthStateProvider.GetAuthenticationStateAsync();
-	            var claimsPrincipal = authenticationState.User;
-	            var userId = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
-	            UserAccountId = userId;
-                await InvokeAsync(StateHasChanged);
-            }
-        }
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				var authenticationState = await AuthStateProvider.GetAuthenticationStateAsync();
+				var claimsPrincipal = authenticationState.User;
+				var userId = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
+				UserAccountId = userId;
+				await InvokeAsync(StateHasChanged);
+			}
+		}
 
-        protected async Task HandleValidSubmit()
-        {
-            var result = await AccountDataService.ChangeUserPasswordAsync(UserAccountId, ChangePasswordModel);
-            if (result.IsSuccessful)
-            {
-                ToastSuccess(result.Message);
-            }
-            else
-            {
-                ToastError(result.Message);
-            }
-        }
-    }
+		protected async Task HandleValidSubmit()
+		{
+			var result = await AccountDataService.ChangeUserPasswordAsync(UserAccountId, ChangePasswordModel);
+			if (result.IsSuccessful)
+			{
+				ToastSuccess(result.Message);
+			}
+			else
+			{
+				ToastError(result.Message);
+			}
+		}
+	}
 }
